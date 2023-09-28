3729c37292c7b9ecae31b3244b1b326b815ec970
Isadora Beckmann <beckmannisadora@gmail.com>
2023-09-28 14:30:04 -0300

    home page

--- gym-typescript/src/App.tsx
+++ gym-typescript/src/App.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/scenes/navbar"
+import Home from "./scenes/home";
 import { useState, useEffect } from "react";
 import { SelectedPage } from "./shared/types";
 
@@ -27,6 +28,8 @@ function App() {
           selectedPage={selectedPage}
           setSelectedPage={setSelectedPage}
          />
+         
+         <Home setSelectedPage={setSelectedPage} />
       </div>
     </>
   );
